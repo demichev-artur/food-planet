2aebdac985cad680f321231f6c7c74c29df48b76
demichev-artur <artur.demichev03@mail.ru>
2022-07-28 09:04:52 +0600

    feat: add footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './Footer.module.css';
+import logo from '../../access/images/Logo.png';
+import phoneIcon from '../../access/icons/phoneIcon.svg';
+
+const Footer = () => {
+    return (
+        <footer>
+            <img src={logo} alt=""/>
+            <ul>
+                <li>Главная</li>
+                <li>Меню</li>
+                <li>Доставка</li>
+                <li>Доставка</li>
+                <li>Контакты</li>
+                <li><img src={phoneIcon} alt=""/>+996500405988</li>
+            </ul>
+        </footer>
+    );
+};
+
+export default Footer;

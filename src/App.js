e287434db901e2c31fe9d86643740c0208947069
demichev-artur <artur.demichev03@mail.ru>
2022-07-30 20:15:10 +0600

    feat: add new products section component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Main from "./pages/Main/Main";
 
 function App() {
   return (
-    <div className="App">
+    <>
         <Main/>
-    </div>
+    </>
   );
 }
 
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
+import MenuNew from "../../components/MenuNew/MenuNew";
 
 const Main = () => {
     return (
         <div>
             <Header/>
+            <MenuNew/>
             <Footer/>
         </div>
     );

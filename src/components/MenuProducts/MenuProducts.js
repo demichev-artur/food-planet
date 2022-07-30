8a6caf88fdf39b738765ef8dcdb5627daabde82f
demichev-artur <artur.demichev03@mail.ru>
2022-07-30 21:35:20 +0600

    feat: add products section component

---
+++ src/components/MenuProducts/MenuProducts.js
@@ -0,0 +1,207 @@
+import React from 'react';
+import styles from "../MenuProducts/MenuProducts.module.css";
+import burgerOne from "../../access/images/Tofu-Burger-1.png";
+import minus from "../../access/icons/minusIcon.svg";
+import plus from "../../access/icons/plusIcon.svg";
+import burgerTwo from "../../access/images/Tofu-Burger-2.png";
+import burgerTree from "../../access/images/Tofu-Burger-3.png";
+import burgerFour from "../../access/images/Tofu-Burger-4.png";
+
+const MenuProducts = () => {
+    return (
+        <div className={styles.menu_new}>
+            <div className={styles.menu_new_nav}>
+                <h1>Меню</h1>
+
+                <ul>
+                    <li>Пицца</li>
+                    <li>Бургер</li>
+                    <li>Суши</li>
+                    <li>Роллы</li>
+                    <li>Салаты</li>
+                    <li>Десерты</li>
+                    <li>Напитки</li>
+                </ul>
+            </div>
+
+            <div className={styles.menu_new_content}>
+                <div className={styles.card}>
+                    <div>
+                        <img src={burgerOne} alt=""/>
+                    </div>
+
+                    <h2>Мексиканская</h2>
+                    <p>Булка, котлета,сыр, соленый огурец, лук, помидор,
+                        салат айсберг,
+                        соус чесночный, соус гриль,
+                        кетчуп, майонез</p>
+                    <h3 className={styles.menu_new_price}>200 сом</h3>
+
+                    <div className={styles.product_count}>
+                        <button><img src={minus} alt=""/></button>
+                        <span>1</span>
+                        <button><img src={plus} alt=""/></button>
+                    </div>
+
+                    <button>В корзину</button>
+                </div>
+                <div className={styles.card}>
+                    <div>
+                        <img src={burgerTwo} alt=""/>
+
+                    </div>
+                    <h2>Мексиканская</h2>
+                    <p>Булка, котлета,сыр, соленый огурец, лук, помидор,
+                        салат айсберг,
+                        соус чесночный, соус гриль,
+                        кетчуп, майонез</p>
+                    <h3 className={styles.menu_new_price}>200 сом</h3>
+
+                    <div className={styles.product_count}>
+                        <button><img src={minus} alt=""/></button>
+                        <span>1</span>
+                        <button><img src={plus} alt=""/></button>
+                    </div>
+                    <button>В корзину</button>
+
+
+                </div>
+                <div className={styles.card}>
+                    <div>
+                        <img src={burgerTree} alt=""/>
+
+                    </div>
+                    <h2>Мексиканская</h2>
+                    <p>Булка, котлета,сыр, соленый огурец, лук, помидор,
+                        салат айсберг,
+                        соус чесночный, соус гриль,
+                        кетчуп, майонез</p>
+                    <h3 className={styles.menu_new_price}>200 сом</h3>
+
+                    <div className={styles.product_count}>
+                        <button><img src={minus} alt=""/></button>
+                        <span>1</span>
+                        <button><img src={plus} alt=""/></button>
+                    </div>
+
+                    <button>В корзину</button>
+
+
+                </div>
+                <div className={styles.card}>
+                    <div>
+                        <img src={burgerFour} alt=""/>
+                    </div>
+
+                    <h2>Мексиканская</h2>
+                    <p>Булка, котлета,сыр, соленый огурец, лук, помидор,
+                        салат айсберг,
+                        соус чесночный, соус гриль,
+                        кетчуп, майонез</p>
+                    <h3 className={styles.menu_new_price}>200 сом</h3>
+
+                    <div className={styles.product_count}>
+                        <button><img src={minus} alt=""/></button>
+                        <span>1</span>
+                        <button><img src={plus} alt=""/></button>
+                    </div>
+
+                    <button>В корзину</button>
+
+
+                </div>
+
+
+                <div className={styles.card}>
+                    <div>
+                        <img src={burgerOne} alt=""/>
+                    </div>
+
+                    <h2>Мексиканская</h2>
+                    <p>Булка, котлета,сыр, соленый огурец, лук, помидор,
+                        салат айсберг,
+                        соус чесночный, соус гриль,
+                        кетчуп, майонез</p>
+                    <h3 className={styles.menu_new_price}>200 сом</h3>
+
+                    <div className={styles.product_count}>
+                        <button><img src={minus} alt=""/></button>
+                        <span>1</span>
+                        <button><img src={plus} alt=""/></button>
+                    </div>
+
+                    <button>В корзину</button>
+                </div>
+                <div className={styles.card}>
+                    <div>
+                        <img src={burgerTwo} alt=""/>
+
+                    </div>
+                    <h2>Мексиканская</h2>
+                    <p>Булка, котлета,сыр, соленый огурец, лук, помидор,
+                        салат айсберг,
+                        соус чесночный, соус гриль,
+                        кетчуп, майонез</p>
+                    <h3 className={styles.menu_new_price}>200 сом</h3>
+
+                    <div className={styles.product_count}>
+                        <button><img src={minus} alt=""/></button>
+                        <span>1</span>
+                        <button><img src={plus} alt=""/></button>
+                    </div>
+                    <button>В корзину</button>
+
+
+                </div>
+                <div className={styles.card}>
+                    <div>
+                        <img src={burgerTree} alt=""/>
+
+                    </div>
+                    <h2>Мексиканская</h2>
+                    <p>Булка, котлета,сыр, соленый огурец, лук, помидор,
+                        салат айсберг,
+                        соус чесночный, соус гриль,
+                        кетчуп, майонез</p>
+                    <h3 className={styles.menu_new_price}>200 сом</h3>
+
+                    <div className={styles.product_count}>
+                        <button><img src={minus} alt=""/></button>
+                        <span>1</span>
+                        <button><img src={plus} alt=""/></button>
+                    </div>
+
+                    <button>В корзину</button>
+
+
+                </div>
+                <div className={styles.card}>
+                    <div>
+                        <img src={burgerFour} alt=""/>
+                    </div>
+
+                    <h2>Мексиканская</h2>
+                    <p>Булка, котлета,сыр, соленый огурец, лук, помидор,
+                        салат айсберг,
+                        соус чесночный, соус гриль,
+                        кетчуп, майонез</p>
+                    <h3 className={styles.menu_new_price}>200 сом</h3>
+
+                    <div className={styles.product_count}>
+                        <button><img src={minus} alt=""/></button>
+                        <span>1</span>
+                        <button><img src={plus} alt=""/></button>
+                    </div>
+
+                    <button>В корзину</button>
+
+
+                </div>
+            </div>
+            <button className={styles.btn_open}>Показать ещё</button>
+        </div>
+
+    );
+};
+
+export default MenuProducts;
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import MenuNew from "../../components/MenuNew/MenuNew";
+import MenuProducts from "../../components/MenuProducts/MenuProducts";
 
 const Main = () => {
     return (
         <div>
             <Header/>
             <MenuNew/>
+            <MenuProducts/>
             <Footer/>
         </div>
     );

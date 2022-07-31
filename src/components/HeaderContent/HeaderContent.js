dae7c2ba6edcb94abd13d654b1783eb10ce4f7cb
demichev-artur <artur.demichev03@mail.ru>
2022-07-31 18:10:55 +0600

    refactor: headerContent.js and style

--- src/components/HeaderContent/HeaderContent.js
+++ src/components/HeaderContent/HeaderContent.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import styles from './HeaderContent.module.css';
-import burgerImg from '../../access/images/header_content_burger.png';
+import burgerImg from '../../access/images/header_content_burger.svg';
 import arrowRightWhite from '../../access/icons/arrowRightWhite.svg';
 
 
 const HeaderContent = () => {
     return (
-        <div className={styles.header_content}>
-            <div>
-                <h1>Доставка вкусной еды до 30 минут + напиток в подарок!</h1>
-                <p>Доставим заказ вовремя и можете рассчитывать, что еда будет доставлен всегда горячим и ароматным.</p>
-                <button>Перейти в меню <img src={arrowRightWhite} alt=""/></button>
-            </div>
+        <section>
+            <div className={styles.content}>
 
-            <img src={burgerImg} alt=""/>
-        </div>
+                <div>
+                    <h1>Доставка вкусной еды до 30 минут + напиток в подарок!</h1>
+                    <p>Доставим заказ вовремя и можете рассчитывать, что еда будет доставлен всегда горячим и ароматным.</p>
+                    <button>Перейти в меню <img src={arrowRightWhite} alt=""/></button>
+                </div>
+
+                <img src={burgerImg} alt=""/>
+            </div>
+        </section>
     );
 };
 

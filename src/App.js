fc08999495d9679ade2fb9d482ba3eed56ede06c
demichev-artur <artur.demichev03@mail.ru>
2022-08-04 23:20:38 +0600

    feat: add NotFound page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Main from "./pages/Main/Main";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Header from "./components/Header/Header";
+import NotFound from "./pages/NotFound/NotFound";
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
                 <Header/>
                 <Routes>
                     <Route path="/" element={<Main/>}/>
+                    <Route path="*" element={<NotFound/>}/>
                 </Routes>
             </BrowserRouter>
 
